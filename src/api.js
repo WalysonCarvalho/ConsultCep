a912b35b70504c4873c084ebd239adb4b14c5960
Walyson Carvalho <walysonmello18@outlook.com>
2024-05-10 12:36:54 -0300

    color button

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 //https://viacep.com.br/ws/01310930/json
 
 const api = axios.create({
-  baseURL: 'https://viacep.com.br/ws/'
+  baseURL: "https://viacep.com.br/ws/",
 });
 
-export default api ;
+export default api;
